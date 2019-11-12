3a92d47718847dd2d6f679c020c95b510e6a621a
Jack-Crum <2629946925@qq.com>
2019-11-12 22:13:34 +0800

    shg

---
+++ 机械弹道实验/MDK-ARM/RM_Template_Gimbal/user_lib.d
@@ -0,0 +1,10 @@
+rm_template_gimbal\user_lib.o: Algorithm\user_lib.c
+rm_template_gimbal\user_lib.o: Algorithm\user_lib.h
+rm_template_gimbal\user_lib.o: D:\Program Files\Keil_v5\ARM\ARM\CMSIS\4.2.0\CMSIS\Include\arm_math.h
+rm_template_gimbal\user_lib.o: D:\Program Files\Keil_v5\ARM\ARM\CMSIS\4.2.0\CMSIS\Include\core_cm4.h
+rm_template_gimbal\user_lib.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+rm_template_gimbal\user_lib.o: D:\Program Files\Keil_v5\ARM\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmInstr.h
+rm_template_gimbal\user_lib.o: D:\Program Files\Keil_v5\ARM\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmFunc.h
+rm_template_gimbal\user_lib.o: D:\Program Files\Keil_v5\ARM\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmSimd.h
+rm_template_gimbal\user_lib.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+rm_template_gimbal\user_lib.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
---
+++ 机械弹道实验/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RM_Template_Gimbal' 
+ * Target:  'RM_Template_Gimbal' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
